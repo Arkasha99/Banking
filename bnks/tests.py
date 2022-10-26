3f0180d83dea1b081fb0e1d9a988e4db0c427d8a
Arkasha99 <GreyForm16@gmail.com>
2022-10-26 17:54:17 +0300

    added test, changed python version

--- bnks/tests.py
+++ bnks/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from . models import Client, Payment, Payback
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def test_post_model(self):
+        client = Client.objects.create(email='123asdas@mail.ru', full_name='Arkadiy Govno')
+        self.assertTrue(isinstance(client, Client))
+        self.assertEqual(client.full_name, 'Arkadiy Govno')
